ea1a92dfd05565f1c5cf7fd5a229de4e5133be8f
hahark-ops <hahark@naver.com>
2026-01-07 09:36:07 +0900

    feat: FastAPI POST 요청 과제 완료

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-	return {"message": "Hello, adepterz!"}
+class UserData(BaseModel):
+    name: str
+    age: int
+    email: str
+
+@app.post("/submit")
+def create_user(user: UserData):
+    return {
+        "status": "success",
+        "message": f"{user.name}님의 데이터를 성공적으로 받았습니다.",
+        "received_data": user
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
