c430cf75eafb95063690752a4b29351355775ffa
hahark-ops <hahark@naver.com>
2026-01-12 16:11:31 +0900

    "refactor: 라우터 통합 관리(index.py) 적용 및 순환 참조 해결"

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # main.py
 
 from fastapi import FastAPI
-# routers 폴더의 post 파일 안에 있는 router 변수를 가져와서 post_router라고 부르겠다.
-from routers.post import router as post_router 
+# [중요] 개별 라우터 import는 다 지우고, index만 가져옵니다.
+from routers.index import router as api_router 
 
 app = FastAPI(title="Community API - Task 2-1")
 
-# [라우터 연결]
-app.include_router(post_router)
+# 통합 라우터 연결
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
---
+++ routers/index.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from routers.post import router as post_router
+# 나중에 user_router, comment_router 등등 생기면 여기서 불러옵니다.
+
+# [통합 라우터 생성]
+# 이 router 변수 하나에 모든 기능이 담깁니다.
+router = APIRouter()
+
+# [하위 라우터 연결]
+router.include_router(post_router)
