f6b8e13ad974a87d2342e08aa0080b90abf4d3d1
hahark-ops <hahark@naver.com>
2026-01-15 14:42:57 +0900

    Fix: Include user router in index (Resolves #2)

--- routers/index.py
+++ routers/index.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from routers.auth import router as auth_router
 from routers.post import router as post_router
 from routers.comment import router as comment_router
+from routers.user import router as user_router
 
 router = APIRouter()
 router.include_router(auth_router)
+router.include_router(user_router)
 router.include_router(post_router)
 router.include_router(comment_router)
